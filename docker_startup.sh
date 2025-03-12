4c464158c3e979e9ac86eb77306d6cb2d7a30840
codigoinerte <fmartinez.bpe@gmail.com>
2025-03-12 15:50:28 -0500

    fix: Increase Gunicorn timeout and update Dockerfile

--- docker_startup.sh
+++ docker_startup.sh
@@ -4,5 +4,5 @@ set -e
 # Ejecuta cualquier comando de configuración adicional aquí
 echo "Starting application..."
 
-# Inicia la aplicación Flask con Gunicorn
-exec gunicorn -w 4 -b 0.0.0.0:8000 main:app
+# Inicia la aplicación Flask con Gunicorn y aumenta el tiempo de espera
+exec gunicorn --timeout 120 -w 4 -b 0.0.0.0:8000 main:app
