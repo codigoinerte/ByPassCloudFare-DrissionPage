4e3f0678b2b4b3cb1d540f3af8f8fc4b869f9331
codigoinerte <fmartinez.bpe@gmail.com>
2025-03-12 14:37:32 -0500

    fix:Update startup script and Dockerfile to run Flask with Gunicorn

--- docker_startup.sh
+++ docker_startup.sh
@@ -4,5 +4,5 @@ set -e
 # Ejecuta cualquier comando de configuración adicional aquí
 echo "Starting application..."
 
-# Inicia la aplicación
-exec gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
+# Inicia la aplicación Flask con Gunicorn
+exec gunicorn -w 4 -b 0.0.0.0:8000 main:app
